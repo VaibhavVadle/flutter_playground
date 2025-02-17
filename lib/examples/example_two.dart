50be87c4bf73e2bf8797fc574d48ed30e1243a9d
Vaibhav Vadle <vaibhavvadle09@gmail.com>
2025-02-18 01:45:36 +0530

    Added custom image transforms and draw atlas

--- lib/examples/example_two.dart
+++ lib/examples/example_two.dart
@@ -44,8 +44,8 @@ class _ExampleTwoState extends State<ExampleTwo> {
         child: Container(
           color: Colors.grey.shade300,
           child: CustomPaint(
-            painter: CustomTextPainter(),
-            // painter: CustomImagePainter(imageInfoNotifier: imageInfoNotifier),
+            // painter: CustomTextPainter(),
+            painter: CustomImagePainter(imageInfoNotifier: imageInfoNotifier),
             size: Size(300, 400),
           ),
         ),
@@ -86,10 +86,50 @@ class CustomImagePainter extends CustomPainter {
     if (image != null) {
       // canvas.drawImage(image, Offset.zero, paint);
       // ---->
-      Size imgSize = Size(image.width.toDouble(), image.height.toDouble());
-      Rect imgRect = Offset.zero & imgSize;
-      Rect canvasRect = Offset.zero & size;
-      canvas.drawImageRect(image, imgRect, canvasRect, paint);
+      // Size imgSize = Size(image.width.toDouble(), image.height.toDouble());
+      // Rect imgRect = Offset.zero & imgSize;
+      // Rect canvasRect = Offset.zero & size;
+      // canvas.drawImageRect(image, imgRect, canvasRect, paint);
+      // ---->
+      List<RSTransform> transforms = [
+        RSTransform.fromComponents(
+          rotation: 0,
+          scale: 1,
+          anchorX: 0,
+          anchorY: 0,
+          translateX: 0,
+          translateY: 0,
+        ),
+        RSTransform.fromComponents(
+          rotation: 0,
+          scale: 1.5,
+          anchorX: 0,
+          anchorY: 0,
+          translateX: 100,
+          translateY: 100,
+        ),
+        RSTransform.fromComponents(
+          rotation: 0.28,
+          scale: 1,
+          anchorX: 0,
+          anchorY: 0,
+          translateX: 200,
+          translateY: 300,
+        ),
+      ];
+
+      List<Rect> rects = [
+        const Rect.fromLTWH(0, 0, 100, 100),
+        const Rect.fromLTWH(100, 100, 100, 100),
+        const Rect.fromLTWH(200, 300, 100, 100),
+      ];
+
+      List<Color> colors = [
+        Colors.white.withOpacity(0.5),
+        Colors.red.withOpacity(0.5),
+        Colors.blue.withOpacity(0.5),
+      ];
+      canvas.drawAtlas(image, transforms, rects, colors, BlendMode.plus, null, paint);
     }
   }
 

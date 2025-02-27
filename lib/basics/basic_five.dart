f7ff6c159e73a67ede341322bc737fd615863426
Vaibhav Vadle <vaibhavvadle09@gmail.com>
2025-02-27 23:48:19 +0530

    Added computeMetrics for contour length, exact subPath, tangent and it's position & vector

--- lib/basics/basic_five.dart
+++ lib/basics/basic_five.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
@@ -19,7 +21,8 @@ class BasicFive extends StatelessWidget {
         child: Container(
           color: Colors.grey.shade300,
           child: CustomPaint(
-            painter: MasterPainter(),
+            painter: ContourPathPainter(),
+            // painter: MasterPainter(),
             size: Size(300, 400),
           ),
         ),
@@ -43,16 +46,57 @@ class MasterPainter extends CustomPainter {
     //   ..lineTo(size.width / 2, size.height / 2)
     //   ..lineTo(size.width, size.height / 2)
     //   ..lineTo(size.width, size.height);
-    path
-      .addPolygon([
-        Offset(0, 0),
-        Offset(size.width / 2, 0),
-        Offset(size.width / 2, size.height / 2),
-        Offset(size.width, size.height / 2),
-        Offset(size.width, size.height),
-      ], false);
+    path.addPolygon([
+      Offset(0, 0),
+      Offset(size.width / 2, 0),
+      Offset(size.width / 2, size.height / 2),
+      Offset(size.width, size.height / 2),
+      Offset(size.width, size.height),
+    ], false);
 
     // ----> Draw canvas with path
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
+
+class ContourPathPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..color = Colors.black
+      ..strokeWidth = 2;
+
+    Rect rect = Rect.fromCircle(
+      center: Offset(size.width / 2, size.height / 2),
+      radius: size.width / 4,
+    );
+    Path path = Path()
+      ..addRect(Rect.fromCenter(
+          center: Offset.zero, width: size.width / 4, height: size.height / 4))
+      ..addOval(rect);
+    // ----> Use of compute metrics to get the area of contour's in path of canvas
+    List<PathMetric> pathMetrics = path.computeMetrics().toList();
+    print(pathMetrics.length);
+    //2*(L+b) = 2*(75+100) = 350
+    print(pathMetrics.first.length);
+    //2*pi*r
+    print(pathMetrics.last.length);
+    Path pathSub = pathMetrics.last.extractPath(10, 100);
+    canvas.drawPath(pathSub, paint);
+    // ----> Tangent used to get the position or angle
+    Tangent? tangent = pathMetrics.last.getTangentForOffset(80);
+    print(tangent?.position);
+    print(tangent?.vector);
+    path.addPolygon([
+      Offset.zero,
+      for(int i=0;i<15;i++) Offset(186.1 + (-0.9 *i*10),265.8 + (0.5*i*10))
+    ], false);
+    // 127
+    print(tangent?.angle);
     canvas.drawPath(path, paint);
   }
 
